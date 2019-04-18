046062cafdb635ea52abbf8e52846ad52d294e76
liurui <1083903857@qq.com>
2019-04-18 16:22:03 +0800

    提交springcloud父工程

---
+++ demo-eurake-server/src/main/java/com/springcloud/eurake/DemoEurakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.eurake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurakeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurakeServerApplication.class, args);
+    }
+
+}
