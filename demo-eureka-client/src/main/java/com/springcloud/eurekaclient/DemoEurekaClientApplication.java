7f6913514cb72486baea2760e734ef75d4eed021
ray <1083903857@qq.com>
2019-04-21 13:20:52 +0800

    父工程完善和demo-eureka-client

---
+++ demo-eureka-client/src/main/java/com/springcloud/eurekaclient/DemoEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DemoEurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaClientApplication.class,args);
+    }
+}
